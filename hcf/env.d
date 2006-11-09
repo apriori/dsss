134ca657d7a261c2d7c4efc67748eaa7ce76a782
Gregor <Gregor@2894d018-631f-0410-8ed0-857076f880b1>
2006-11-09 23:32:05 +0000

    Changed everything to MIT license.
    git-svn-id: http://svn.dsource.org/projects/dsss/trunk@86 2894d018-631f-0410-8ed0-857076f880b1

--- hcf/env.d
+++ hcf/env.d
@@ -5,6 +5,8 @@
  *  Gregor Richards
  * 
  * License:
+ *  Copyright (c) 2006  Gregor Richards
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a
  *  copy of this software and associated documentation files (the "Software"),
  *  to deal in the Software without restriction, including without limitation
