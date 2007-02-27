54195cdf8d2826d197797268c94b13b9c683b39b
Gregor <Gregor@2894d018-631f-0410-8ed0-857076f880b1>
2007-02-27 06:53:26 +0000

    RELEASE: 0.55
    RELEASE: rebuild 0.13
    
    
    
    git-svn-id: http://svn.dsource.org/projects/dsss/trunk@404 2894d018-631f-0410-8ed0-857076f880b1

--- rebuild/mars.c
+++ rebuild/mars.c
@@ -63,7 +63,7 @@ Global::Global()
 
     copyright = "Copyright (c) 1999-2007 by Digital Mars and Gregor Richards";
     written = "written by Walter Bright and Gregor Richards";
-    version = "version 0.12 (based on DMD 1.006)";
+    version = "version 0.13 (based on DMD 1.006)";
     global.structalign = 8;
     cmodules = NULL;
 
--- sss/main.d
+++ sss/main.d
@@ -43,7 +43,7 @@ version (DSSS_Light) {} else {
 }
 import sss.uninstall;
 
-const char[] DSSS_VERSION = "0.54";
+const char[] DSSS_VERSION = "0.55";
 
 private {
     /** Possible commands */
