4da21200bc47f93440750f8b12fe9f7008a9286c
Gregor <Gregor@2894d018-631f-0410-8ed0-857076f880b1>
2007-03-28 23:52:26 +0000

    rebuild/util/fdt.d: Apparently OS X uses std.c.unix.unix only, not std.c.darwin.darwin, at least on some systems. One way or another, it can't hurt.
    git-svn-id: http://svn.dsource.org/projects/dsss/trunk@432 2894d018-631f-0410-8ed0-857076f880b1

--- rebuild/util/fdt.d
+++ rebuild/util/fdt.d
@@ -67,7 +67,7 @@ private
     }
     version(Windows)      static import opsys = std.c.windows.windows;
     else version(linux)   static import opsys = std.c.linux.linux;
-    else version(darwin)  static import opsys = std.c.darwin.darwin;
+    else version(darwin)  static import opsys = std.c.unix.unix;
     else version(Unix)    static import opsys = std.c.unix.unix;
 
     version(Posix)   static import std.string;
