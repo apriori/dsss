fb50028aef578c9eb14fe2822f35c93bd2884899
Gregor <Gregor@2894d018-631f-0410-8ed0-857076f880b1>
2007-04-16 19:07:32 +0000

    RELEASE: 0.61
    git-svn-id: http://svn.dsource.org/projects/dsss/trunk@494 2894d018-631f-0410-8ed0-857076f880b1

--- rebuild/mars.c
+++ rebuild/mars.c
@@ -64,7 +64,7 @@ Global::Global()
 
     copyright = "Copyright (c) 1999-2007 by Digital Mars and Gregor Richards";
     written = "written by Walter Bright and Gregor Richards";
-    version = "version 0.18 (based on DMD 1.012)";
+    version = "version 0.61 (based on DMD 1.012)";
     global.structalign = 8;
     cmodules = NULL;
 
